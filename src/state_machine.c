41863c2b2d43f20b84c728c4db109902aceb2c62
Jonathan Armstrong <jda2158@gmail.com>
2024-06-10 12:52:43 -0500

    working on RF out

--- src/state_machine.c
+++ src/state_machine.c
@@ -7,25 +7,37 @@
 #include <stdio.h>
 #include <stdint.h>
 
+// hold past bytes so we can parse when sync end is received
+#define PACKET_MAX_SIZE  12
+
 //-----------------------------------------------------------------------------
 // Portisch generally used this type of state machine, which I had a lot of trouble reading
 //-----------------------------------------------------------------------------
-void uart_state_machine(const unsigned int rxdata)
+void uart_state_machine(const unsigned int rxdataWithFlags)
 {
     // FIXME: need to check what appropriate initialization values are
     static UART_STATE_T state = IDLE;
-    UART_COMMAND_T command = NONE;
-    
-    uint16_t bucket = 0;
+    static UART_COMMAND_T command = NONE;
 
+	static uint8_t packet[PACKET_MAX_SIZE];
+	
     // FIXME: need to know what initialization value is appropriate
-    uint8_t position = 0;
-    
+    static uint8_t position = 0;
+	static uint8_t lengthExpected = 0;
+
+    // FIXME: needed?
+    //uint16_t bucket = 0;
+	
+	// strip uart flags to 
+	const uint8_t rxdata = rxdata & 0xFF;
+
+
     // track count of entries into function
     static uint16_t idleResetCount = 0;
 
     // FIXME: this seems to reset state machine if we do not receive data after some time
-    if (rxdata == UART_NO_DATA)
+	// FIXME: but we need to determine this outside of state machine I think ?
+    if (rxdataWithFlags == UART_NO_DATA)
     {
         if (state == IDLE)
             idleResetCount = 0;
@@ -54,19 +66,23 @@ void uart_state_machine(const unsigned int rxdata)
     {
         // check if UART_SYNC_INIT got received
         case IDLE:
-            if ((rxdata & 0xFF) == RF_CODE_START)
+            if (rxdata == RF_CODE_START)
             {
                 state = SYNC_INIT;
+				
+				// go ahead and store the sync start (should be 0xAA)
+				packet[position] = SYNC_INIT;
+				position++;
             }
             
             break;
 
-        // sync byte got received, read command
+        // sync byte got received, so next read command
         case SYNC_INIT:
-            command = rxdata & 0xFF;
+            command = rxdata;
             
             // FIXME: not sure if setting this here is correct
-            state = SYNC_FINISH;
+            //state = SYNC_FINISH;
 
             // check if some data needs to be received
             switch(command)
@@ -77,6 +93,11 @@ void uart_state_machine(const unsigned int rxdata)
                 case RF_CODE_RFIN:
                     break;
                 case RF_CODE_RFOUT:
+					packet[position] = command;
+					position++;
+					
+					lengthExpected = 11;
+					state = RECEIVING;
                     break;
                 case RF_DO_BEEP:
                     // FIXME: replace with timer rather than delay(), although appears original code was blocking too
@@ -86,10 +107,14 @@ void uart_state_machine(const unsigned int rxdata)
 
                     // send acknowledge
                     uart_put_command(RF_CODE_ACK);
+					
+					state = SYNC_FINISH;
                     break;
                 case RF_ALTERNATIVE_FIRMWARE:
                     uart_put_command(RF_CODE_ACK);
                     uart_put_command(FIRMWARE_VERSION);
+					
+					state = SYNC_FINISH;
                     break;
                 case RF_CODE_SNIFFING_ON:
                     //gSniffingMode = ADVANCED;
@@ -151,28 +176,85 @@ void uart_state_machine(const unsigned int rxdata)
 
         // receiving UART data
         case RECEIVING:
-            //gRFData[position] = rxdata & 0xFF;
-            //position++;
+            packet[position] = rxdata;
+            position++;
 
-            //if (position == gLength)
-            //{
-            //    state = SYNC_FINISH;
-            //}
-            //else if (position >= RF_DATA_BUFFERSIZE)
-            //{
-            //    gLength = RF_DATA_BUFFERSIZE;
-            //    state = SYNC_FINISH;
-            //}
+            if (position == lengthExpected)
+            {
+                state = SYNC_FINISH;
+            }
+            else if (position >= PACKET_MAX_SIZE)
+            {
+				// FIXME: review this logic
+                lengthExpected = PACKET_MAX_SIZE;
+                state = SYNC_FINISH;
+            }
             break;
 
         // wait and check for UART_SYNC_END
         case SYNC_FINISH:
-            if ((rxdata & 0xFF) == RF_CODE_STOP)
+            if (rxdata == RF_CODE_STOP)
             {
                 state = IDLE;
             }
             break;
     }
+	
+	// this should be occuring after an entire uart packet is received (i.e., after SYNC_FINISH)
+	switch(command)
+	{
+		case RF_CODE_RFOUT:
+		
+				// only do the job if all data got received by UART
+				if (state != IDLE)
+					break;
+
+				// do transmit of the data
+				switch(rf_state)
+				{
+					// init and start RF transmit
+					case RF_IDLE:
+						transmissionRepeats--;
+						//PCA0_StopSniffing();
+
+						// bytes 0..1:	Tsyn
+						// bytes 2..3:	Tlow
+						// bytes 4..5:	Thigh
+						// bytes 6..7:	24bit Data
+						buckets[0] = *(uint16_t *)&packet[2];
+						buckets[1] = *(uint16_t *)&packet[4];
+						buckets[2] = *(uint16_t *)&packet[0];
+
+						FIXME: send
+						//SendBuckets(
+						//		buckets,
+						//		PROTOCOL_DATA[0].start.dat, PROTOCOL_DATA[0].start.size,
+						//		PROTOCOL_DATA[0].bit0.dat, PROTOCOL_DATA[0].bit0.size,
+						//		PROTOCOL_DATA[0].bit1.dat, PROTOCOL_DATA[0].bit1.size,
+						//		PROTOCOL_DATA[0].end.dat, PROTOCOL_DATA[0].end.size,
+						//		PROTOCOL_DATA[0].bit_count,
+						//		RF_DATA + 6
+						//		);
+						break;
+
+					// wait until data got transfered
+					case RF_FINISHED:
+						// FIXME: how do we ever get to this case in original portisch?
+						if (transmissionRepeats == 0)
+						{
+							// disable RF transmit
+							tdata_off();
+
+							finish_command(RF_CODE_ACK);
+						}
+						else
+						{
+							rf_state = RF_IDLE;
+						}
+						break;
+				}
+				break;
+	}
 }
 
 
