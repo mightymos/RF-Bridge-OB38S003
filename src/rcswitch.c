272fbc8460d451e19527721f3d0a7d3f3ec968eb
Jonathan Armstrong <jda2158@gmail.com>
2024-06-20 01:30:39 -0500

    store units of 10 microseconds for protocols instead of 1 microsecond to reduce math

--- src/rcswitch.c
+++ src/rcswitch.c
@@ -21,18 +21,18 @@ __xdata int nRepeatTransmit = 8;
 
 //
 const struct Protocol protocols[] = {
-  { 350, {  1, 31 }, {  1,  3 }, {  3,  1 }, false },    // protocol 1
-  { 650, {  1, 10 }, {  1,  2 }, {  2,  1 }, false },    // protocol 2
-  { 100, { 30, 71 }, {  4, 11 }, {  9,  6 }, false },    // protocol 3
-  { 380, {  1,  6 }, {  1,  3 }, {  3,  1 }, false },    // protocol 4
-  { 500, {  6, 14 }, {  1,  2 }, {  2,  1 }, false },    // protocol 5
-  { 450, { 23,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 6 (HT6P20B)
-  { 150, {  2, 62 }, {  1,  6 }, {  6,  1 }, false },    // protocol 7 (HS2303-PT, i. e. used in AUKEY Remote)
-  { 200, {  3, 130}, {  7, 16 }, {  3,  16}, false},     // protocol 8 Conrad RS-200 RX
-  { 200, { 130, 7 }, {  16, 7 }, { 16,  3 }, true},      // protocol 9 Conrad RS-200 TX
-  { 365, { 18,  1 }, {  3,  1 }, {  1,  3 }, true },     // protocol 10 (1ByOne Doorbell)
-  { 270, { 36,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 11 (HT12E)
-  { 320, { 36,  1 }, {  1,  2 }, {  2,  1 }, true }      // protocol 12 (SM5212)
+  { 35, {  1, 31 }, {  1,  3 }, {  3,  1 }, false },    // protocol 1
+  { 65, {  1, 10 }, {  1,  2 }, {  2,  1 }, false },    // protocol 2
+  { 10, { 30, 71 }, {  4, 11 }, {  9,  6 }, false },    // protocol 3
+  { 38, {  1,  6 }, {  1,  3 }, {  3,  1 }, false },    // protocol 4
+  { 50, {  6, 14 }, {  1,  2 }, {  2,  1 }, false },    // protocol 5
+  { 45, { 23,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 6 (HT6P20B)
+  { 15, {  2, 62 }, {  1,  6 }, {  6,  1 }, false },    // protocol 7 (HS2303-PT, i. e. used in AUKEY Remote)
+  { 20, {  3, 130}, {  7, 16 }, {  3,  16}, false},     // protocol 8 Conrad RS-200 RX
+  { 20, { 130, 7 }, {  16, 7 }, { 16,  3 }, true},      // protocol 9 Conrad RS-200 TX
+  { 36, { 18,  1 }, {  3,  1 }, {  1,  3 }, true },     // protocol 10 (1ByOne Doorbell) (rounded)
+  { 27, { 36,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 11 (HT12E)
+  { 32, { 36,  1 }, {  1,  2 }, {  2,  1 }, true }      // protocol 12 (SM5212)
 };
 
 
@@ -288,26 +288,22 @@ void transmit(struct Protocol *pro, struct HighLow pulses, uint16_t highdelay, u
 
 	//__xdata uint16_t previous;
 	//__xdata uint16_t elapsed;
-	//__xdata uint16_t delay;
+	__xdata uint16_t delay;
   
     set_tdata(firstLogicLevel);
 	// mirror transmitted pulses to another pin for easier probing by oscilloscope
 	set_debug_pin01(firstLogicLevel);
 	//delay_us(pro->pulseLength * pulses.high);
-	//delay = pro->pulseLength * pulses.high;
-	//init_timer1_us(1, highdelay);
-	//wait_timer1_finished();
-	init_delay_timer_us(1, highdelay);
+	delay = pro->pulseLength * pulses.high;
+	init_delay_timer_us(1, delay);
 	wait_delay_timer_finished();
 
 
     set_tdata(secondLogicLevel);
     set_debug_pin01(secondLogicLevel);
 	//delay_us(pro->pulseLength * pulses.low);
-	//delay = pro->pulseLength * pulses.low;
-	//init_timer1_us(1, lowdelay);
-	//wait_timer1_finished();
-	init_delay_timer_us(1, lowdelay);
+	delay = pro->pulseLength * pulses.low;
+	init_delay_timer_us(1, delay);
 	wait_delay_timer_finished();
 }
 
