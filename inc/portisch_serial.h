455b973f669941b541d4bc50c239a8d148e880aa
Jonathan Armstrong <jda2158@gmail.com>
2024-12-07 11:36:38 -0600

    remove commented out code

--- inc/portisch_serial.h
+++ inc/portisch_serial.h
@@ -6,10 +6,7 @@
 //-----------------------------------------------------------------------------
 // Global Variables
 //-----------------------------------------------------------------------------
-//extern __xdata uart_state_t uart_state;
-//extern __xdata uart_command_t uart_command;
 
-//extern void uart_put_command(uint8_t command);
 
 extern void uart_put_RF_Data_Standard(uint8_t command);
 extern void uart_put_RF_Data_Advanced(uint8_t command, uint8_t protocol_index);
